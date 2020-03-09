8617cfe7b440e9531bda81421a92a9f30b2e415d
Yuliana.Nesterovich <Yuliana.Nesterovich>
2020-03-09 17:42:51 +0300

    Added chat page tests: sendLongMessage, sendLink, sendHTMLcode, editMessage, checkElevenMessages

--- src/test/java/pages/ChatPage.java
+++ src/test/java/pages/ChatPage.java
@@ -50,7 +50,34 @@ public class ChatPage extends BasePage {
         boolean isExist = message.get(messageIndex - 1).getText().equals(text);
         assertTrue(isExist, "Message do not exist");
     }
-    public void clickInvite(){
+
+    public void linkShouldExist(String text) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".integri-chat-message-attachment-link")));
+        boolean isExistLink = driver.findElement(By.xpath("//div[@class=\"integri-chat-message-attachment integri-chat-message-attachment-link\"]")).getText().contains(text);
+        assertTrue(isExistLink, "Message do not exist");
+    }
+
+    public void clickEdit(int messageIndex) {
+        List<WebElement> iconEdit = driver.findElements(By.cssSelector(".integri-chat-edit-message"));
+        iconEdit.get(messageIndex-1).click();
+    }
+    public void changeTextMessage(int messageIndex, String text) {
+        List<WebElement> messageEdit = driver.findElements(By.xpath("//div[@class =\"integri-chat-message \"]/textarea"));
+        messageEdit.get(messageIndex-1).clear();
+        messageEdit.get(messageIndex-1).sendKeys(text);
+        wait.until(ExpectedConditions.textToBe(By.xpath("//div[@class =\"integri-chat-message \"]/textarea"), text));
+        clickEnter();
+    }
+
+    public void SendElevenMessages(String text) {
+        for (int i = 0; i < 11; i++) {
+            writeText(text);
+            wait.until(ExpectedConditions.textToBe(CHAT_TEXTAREA, text));
+            clickSend();
+        }
+    }
+
+    public void clickInvite() {
         driver.findElement(By.id("invite-users-to-chat")).click();
         try {
             String link = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
--- src/test/java/tests/ChatTests.java
+++ src/test/java/tests/ChatTests.java
@@ -20,6 +20,51 @@ public class ChatTests extends BaseTest {
         chatPage.clickEnter();
         chatPage.messageShouldExist(1, "Text");
     }
+
+    @Test
+    public void sendLongMessage() {
+        chatPage.openPage();
+        chatPage.isChatOpened();
+        chatPage.writeText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula est, ultrices sodales mi eu, accumsan efficitur nisl. Mauris rutrum magna id justo malesuada, nec dapibus est faucibus. In hac habitasse platea dictumst. Suspendisse velit ligula, fermentum sit amet erat ut, fringilla dapibus leo. Integer sagittis nibh lectus, commodo convallis lorem facilisis id. Suspendisse sit amet mollis est, sed condimentum sapien. Suspendisse semper, libero et egestas tempor, odio enim rutrum massa, sit amet posuere nisl mi venenatis eros. Nulla commodo, risus id pulvinar suscipit, odio enim tristique magna, eget dapibus tortor libero vitae ante. Donec enim turpis, malesuada eget viverra a, convallis eu ante. Donec dolor elit, consectetur sed felis ac, euismod pellentesque nunc. Praesent posuere risus quis pellentesque placerat. Quisque enim ante, luctus eu lectus eu, hendrerit congue massa. Curabitur diam arcu, pellentesque eget velit et, porta suscipit erat. Vivamus semper cursus rutrum.12");
+        chatPage.clickEnter();
+        chatPage.messageShouldExist(1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula est, ultrices sodales mi eu, accumsan efficitur nisl. Mauris rutrum magna id justo malesuada, nec dapibus est faucibus. In hac habitasse platea dictumst. Suspendisse velit ligula, fermentum sit amet erat ut, fringilla dapibus leo. Integer sagittis nibh lectus, commodo convallis lorem facilisis id. Suspendisse sit amet mollis est, sed condimentum sapien. Suspendisse semper, libero et egestas tempor, odio enim rutrum massa, sit amet posuere nisl mi venenatis eros. Nulla commodo, risus id pulvinar suscipit, odio enim tristique magna, eget dapibus tortor libero vitae ante. Donec enim turpis, malesuada eget viverra a, convallis eu ante. Donec dolor elit, consectetur sed felis ac, euismod pellentesque nunc. Praesent posuere risus quis pellentesque placerat. Quisque enim ante, luctus eu lectus eu, hendrerit congue massa. Curabitur diam arcu, pellentesque eget velit et, porta suscipit erat. Vivamus semper cursus rutrum.12");
+    }
+
+    @Test
+    public void sendLink() {
+        chatPage.openPage();
+        chatPage.isChatOpened();
+        chatPage.writeText("http://onliner.by");
+        chatPage.clickEnter();
+        chatPage.messageShouldExist(1, "http://onliner.by");
+        chatPage.linkShouldExist("Onliner посещают сотни тысяч белорусов, чтобы покупать в каталоге, размещать объявления, общаться на форуме, а также читать новости и статьи!");
+    }
+
+    @Test
+    public void sendHTMLcode() {
+        chatPage.openPage();
+        chatPage.isChatOpened();
+        chatPage.writeText("<html><body><p>test</p></body></html>");
+        chatPage.clickEnter();
+        chatPage.messageShouldExist(1, "<html><body><p>test</p></body></html>");
+    }
+
+    @Test
+    public void editMessage() {
+        chatPage.openPage();
+        chatPage.isChatOpened();
+        chatPage.writeText("Hello");
+        chatPage.clickEnter();
+        chatPage.clickEdit(1);
+        chatPage.changeTextMessage(1,"GoodBuy");
+        chatPage.messageShouldExist(1, "GoodBuy");
+    }
+    @Test
+    public void checkElevenMessages() {
+        chatPage.openPage();
+        chatPage.isChatOpened();
+        chatPage.SendElevenMessages("Hello");
+    }
     @Test
     public void checkInvite() {
         chatPage.openPage();
