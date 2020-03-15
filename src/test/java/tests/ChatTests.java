f505f350b256ff85fef4d5e258681f16c1520c55
Yuliana.Nesterovich <Yuliana.Nesterovich>
2020-03-15 13:39:23 +0300

    Added variables

--- src/test/java/tests/ChatTests.java
+++ src/test/java/tests/ChatTests.java
@@ -3,68 +3,76 @@ package tests;
 import org.testng.annotations.Test;
 
 public class ChatTests extends BaseTest {
-    @Test
+    private String longMessage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula est, ultrices sodales mi eu, accumsan efficitur nisl. Mauris rutrum magna id justo malesuada, nec dapibus est faucibus. In hac habitasse platea dictumst. Suspendisse velit ligula, fermentum sit amet erat ut, fringilla dapibus leo. Integer sagittis nibh lectus, commodo convallis lorem facilisis id. Suspendisse sit amet mollis est, sed condimentum sapien. Suspendisse semper, libero et egestas tempor, odio enim rutrum massa, sit amet posuere nisl mi venenatis eros. Nulla commodo, risus id pulvinar suscipit, odio enim tristique magna, eget dapibus tortor libero vitae ante. Donec enim turpis, malesuada eget viverra a, convallis eu ante. Donec dolor elit, consectetur sed felis ac, euismod pellentesque nunc. Praesent posuere risus quis pellentesque placerat. Quisque enim ante, luctus eu lectus eu, hendrerit congue massa. Curabitur diam arcu, pellentesque eget velit et, porta suscipit erat. Vivamus semper cursus rutrum.12";
+    private String link = "http://onliner.by";
+    private String siteDescription = "Onliner посещают сотни тысяч белорусов, чтобы покупать в каталоге, размещать объявления, общаться на форуме, а также читать новости и статьи!";
+    private String htmlCode  = "<html><body><p>test</p></body></html>";
+    private String textMessage  = "Text";
+
+    @Test // done
     public void sendMessageByClick() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("Text");
+        chatPage.writeText(textMessage);
         chatPage.clickSend();
-        chatPage.messageShouldExist(1, "Text");
+        chatPage.messageShouldExist(1, textMessage);
     }
 
-    @Test
+    @Test //done
     public void sendMessageByEnter() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("Text");
+        chatPage.writeText(textMessage);
         chatPage.clickEnter();
-        chatPage.messageShouldExist(1, "Text");
+        chatPage.messageShouldExist(1, textMessage);
     }
 
-    @Test
+    @Test //done
     public void sendLongMessage() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula est, ultrices sodales mi eu, accumsan efficitur nisl. Mauris rutrum magna id justo malesuada, nec dapibus est faucibus. In hac habitasse platea dictumst. Suspendisse velit ligula, fermentum sit amet erat ut, fringilla dapibus leo. Integer sagittis nibh lectus, commodo convallis lorem facilisis id. Suspendisse sit amet mollis est, sed condimentum sapien. Suspendisse semper, libero et egestas tempor, odio enim rutrum massa, sit amet posuere nisl mi venenatis eros. Nulla commodo, risus id pulvinar suscipit, odio enim tristique magna, eget dapibus tortor libero vitae ante. Donec enim turpis, malesuada eget viverra a, convallis eu ante. Donec dolor elit, consectetur sed felis ac, euismod pellentesque nunc. Praesent posuere risus quis pellentesque placerat. Quisque enim ante, luctus eu lectus eu, hendrerit congue massa. Curabitur diam arcu, pellentesque eget velit et, porta suscipit erat. Vivamus semper cursus rutrum.12");
+        chatPage.writeText(longMessage);
         chatPage.clickEnter();
-        chatPage.messageShouldExist(1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula est, ultrices sodales mi eu, accumsan efficitur nisl. Mauris rutrum magna id justo malesuada, nec dapibus est faucibus. In hac habitasse platea dictumst. Suspendisse velit ligula, fermentum sit amet erat ut, fringilla dapibus leo. Integer sagittis nibh lectus, commodo convallis lorem facilisis id. Suspendisse sit amet mollis est, sed condimentum sapien. Suspendisse semper, libero et egestas tempor, odio enim rutrum massa, sit amet posuere nisl mi venenatis eros. Nulla commodo, risus id pulvinar suscipit, odio enim tristique magna, eget dapibus tortor libero vitae ante. Donec enim turpis, malesuada eget viverra a, convallis eu ante. Donec dolor elit, consectetur sed felis ac, euismod pellentesque nunc. Praesent posuere risus quis pellentesque placerat. Quisque enim ante, luctus eu lectus eu, hendrerit congue massa. Curabitur diam arcu, pellentesque eget velit et, porta suscipit erat. Vivamus semper cursus rutrum.12");
+        chatPage.messageShouldExist(1, longMessage);
     }
 
-    @Test
+    @Test // done
     public void sendLink() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("http://onliner.by");
+        chatPage.writeText(link);
         chatPage.clickEnter();
-        chatPage.messageShouldExist(1, "http://onliner.by");
-        chatPage.linkShouldExist("Onliner посещают сотни тысяч белорусов, чтобы покупать в каталоге, размещать объявления, общаться на форуме, а также читать новости и статьи!");
+        chatPage.messageShouldExist(1, link);
+        chatPage.linkShouldExist(siteDescription);
     }
 
-    @Test
+    @Test //done
     public void sendHTMLcode() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("<html><body><p>test</p></body></html>");
+        chatPage.writeText(htmlCode);
         chatPage.clickEnter();
-        chatPage.messageShouldExist(1, "<html><body><p>test</p></body></html>");
+        chatPage.messageShouldExist(1, htmlCode);
     }
 
-    @Test
+    @Test //do not done
     public void editMessage() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("Hello");
+        chatPage.writeText(textMessage);
         chatPage.clickEnter();
         chatPage.clickEdit(1);
-        chatPage.changeTextMessage(1,"GoodBuy");
-        chatPage.messageShouldExist(1, "GoodBuy");
+        chatPage.clearTextMessage(1);
+        chatPage.changeMessageText(1, textMessage + "change");
+        chatPage.clickEnter();
+        chatPage.messageShouldExist(1, textMessage + "change");
     }
 
     @Test //done
     public void clearMessage() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("Hello");
+        chatPage.writeText(textMessage);
         chatPage.clickEnter();
         chatPage.clickEdit(1);
         chatPage.clearTextMessage(1);
@@ -76,7 +84,7 @@ public class ChatTests extends BaseTest {
     public void deleteMessage() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.writeText("Hello");
+        chatPage.writeText(textMessage);
         chatPage.clickEnter();
         chatPage.clickDelete(1);
         chatPage.confirmAlert();
@@ -87,7 +95,7 @@ public class ChatTests extends BaseTest {
     public void checkElevenMessages() {
         chatPage.openPage();
         chatPage.isChatOpened();
-        chatPage.SendMoreThanTenMessages("Hello", 11);
+        chatPage.SendMoreThanTenMessages(textMessage, 11);
         chatPage.checkWarningTrialPeriod();
     }
 
