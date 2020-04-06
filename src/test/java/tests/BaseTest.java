a4cd6a02e38069b4e8f41b2b6831c804606406ef
Yuliana.Nesterovich <Yuliana.Nesterovich>
2020-04-06 20:27:06 +0300

    Added Before, After Method

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import pages.*;
 import steps.LoginSteps;
@@ -20,7 +22,7 @@ public class BaseTest {
     LoginSteps loginSteps;
     ProjectPage projectPage;
 
-    @BeforeTest
+    @BeforeMethod
     public void setDriver(){
 //        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
@@ -35,7 +37,7 @@ public class BaseTest {
         projectPage = new ProjectPage(driver);
     }
 
-    @AfterTest(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void closeDriver() {
         driver.quit();
     }
